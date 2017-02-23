455d8ea2fdd93fe4c4602b1cdcd403781d08d9d3
Stephen Wond <stephenwond@paypoint.com>
2017-02-23 17:03:14 +0000

    Looking at every message now

--- HTMLTool/HTMLToolLogic/Logic.cs
+++ HTMLTool/HTMLToolLogic/Logic.cs
@@ -41,14 +41,8 @@ namespace HTMLToolLogic
 
             resultsList.Add(GetResult(_oneOneList, _oneTwoList));
             resultsList.Add(GetResult(_twoOneList, _twoTwoList));
-            //resultsList.Add(GetResult(_threeOneList, _threeTwoList));
-           // resultsList.Add(GetResult(_fourOneList, _fourTwoList));
-
-            //find times
-            //resultsList.Add(GetResult(_one, _request1));
-            //resultsList.Add(GetResult(_response1, _oneTen));
-            //resultsList.Add(GetResult(_two, _request2));
-            //resultsList.Add(GetResult(_response2, _twoTen));
+            resultsList.Add(GetResult(_threeOneList, _threeTwoList));
+            resultsList.Add(GetResult(_fourOneList, _fourTwoList));
 
             return resultsList;
         }
@@ -70,14 +64,14 @@ namespace HTMLToolLogic
                 try
                 {
                     doc.Load(file);
-                    _oneOneList.AddRange  (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where !node.InnerText.Contains("/api/serverstatus.aspx") && !node.InnerText.Contains("STATUS=OK") where node.InnerText.Contains(_oneOne) select node.InnerText);
-                    _oneTwoList.AddRange  (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where !node.InnerText.Contains("/api/serverstatus.aspx") && !node.InnerText.Contains("STATUS=OK") where node.InnerText.Contains(_oneTwo) && node.InnerText.Contains("ENQPBP") select node.InnerText);
-                    _twoOneList.AddRange  (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where !node.InnerText.Contains("/api/serverstatus.aspx") && !node.InnerText.Contains("STATUS=OK") where node.InnerText.Contains(_twoOne) && node.InnerText.Contains("ENQPBP") select node.InnerText);
-                    _twoTwoList.AddRange  (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where !node.InnerText.Contains("/api/serverstatus.aspx") && !node.InnerText.Contains("STATUS=OK") where node.InnerText.Contains(_twoTwo) select node.InnerText);
-                    _threeOneList.AddRange(from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where !node.InnerText.Contains("/api/serverstatus.aspx") && !node.InnerText.Contains("STATUS=OK") where node.InnerText.Contains(_threeOne) select node.InnerText);
-                    _threeTwoList.AddRange(from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where !node.InnerText.Contains("/api/serverstatus.aspx") && !node.InnerText.Contains("STATUS=OK") where node.InnerText.Contains(_threeTwo) && node.InnerText.Contains("VALPBP") select node.InnerText);
-                    _fourOneList.AddRange (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where !node.InnerText.Contains("/api/serverstatus.aspx") && !node.InnerText.Contains("STATUS=OK") where node.InnerText.Contains(_fourOne) && node.InnerText.Contains("VALPBP") select node.InnerText);
-                    _fourTwoList.AddRange (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where !node.InnerText.Contains("/api/serverstatus.aspx") && !node.InnerText.Contains("STATUS=OK") where node.InnerText.Contains(_fourTwo) select node.InnerText);
+                    _oneOneList.AddRange  (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where node.InnerText.Contains(_oneOne) select node.InnerText);
+                    _oneTwoList.AddRange  (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where node.InnerText.Contains(_oneTwo) && node.InnerText.Contains("ENQPBP") select node.InnerText);
+                    _twoOneList.AddRange  (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where node.InnerText.Contains(_twoOne) && node.InnerText.Contains("ENQPBP") select node.InnerText);
+                    _twoTwoList.AddRange  (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where node.InnerText.Contains(_twoTwo) select node.InnerText);
+                    _threeOneList.AddRange(from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where node.InnerText.Contains(_threeOne) select node.InnerText);
+                    _threeTwoList.AddRange(from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where node.InnerText.Contains(_threeTwo) && node.InnerText.Contains("VALPBP") select node.InnerText);
+                    _fourOneList.AddRange (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where node.InnerText.Contains(_fourOne) && node.InnerText.Contains("VALPBP") select node.InnerText);
+                    _fourTwoList.AddRange (from node in doc.DocumentNode.SelectNodes("//div[@id='postA']") where node.InnerText.Contains(_fourTwo) select node.InnerText);
                 }
                 catch
                 {
@@ -99,11 +93,11 @@ namespace HTMLToolLogic
                 var pbpStart = x.IndexOf("PBP", 0);
                 var pbpString = x.Substring(pbpStart, 12);
 
-                foreach (var y in to)
-                {
-                    if (y.Contains(pbpString))
+                for (int i = 0; i < to.Count; i++)
+               {
+                    if (to[i].Contains(pbpString))
                     {
-                        var diff = FindDifference(x.Substring(1, 19),y.Substring(1, 19));
+                        var diff = FindDifference(x.Substring(1, 19), to[i].Substring(1, 19));
                         result.Average = result.Average + diff;
                         result.Count++;
 
@@ -115,60 +109,17 @@ namespace HTMLToolLogic
                         {
                             result.Max = diff;
                         }
+                        to.Remove(to[i]);
                         break;
                     }
                 }
             }
             result.Average = Math.Round((result.Average / result.Count), 2);
-            result.Direction = from.ToString().Substring(4, 4) + "->" + to.ToString().Substring(4, 4);
+            result.Direction = nameof(from) + "->" + nameof(to);
 
             return result;
 
         }
-
-        //private Result GetResult(string from, string to)
-        //{
-        //    var result = new Result();
-        //    var userNumber = int.Parse(ConfigurationManager.AppSettings["MessageForward"]);
-        //    result.Max = 0;
-        //    result.Min = 100;
-
-        //    //check next few messages
-        //    for (var i = 0; i < _nodeList.Count; i++)
-        //    {
-        //        if (_nodeList[i].Contains(from))
-        //        {
-        //            var k = i + 1;
-        //            while (k < i + 1 + userNumber && k < _nodeList.Count)
-        //            {
-        //                if (!_nodeList[k].Contains(to))
-        //                {
-        //                    k++;
-        //                    continue;
-        //                }
-        //                result.Count++;
-        //                var difference = FindDifference(_nodeList[i].Substring(1, 19),
-        //                    _nodeList[k].Substring(1, 19));
-
-        //                result.Average = result.Average + difference;
-
-        //                if (difference < result.Min)
-        //                {
-        //                    result.Min = difference;
-        //                }
-        //                if (difference > result.Max)
-        //                {
-        //                    result.Max = difference;
-        //                }
-        //                break;
-        //            }
-        //        }
-        //    }
-        //    result.Average = Math.Round((result.Average / result.Count),2);
-        //    result.Direction = from.Substring(4, 4) + "->" + to.Substring(4, 4);
-
-        //    return result;
-        //}
 
         private static double FindDifference(string from, string to)
         {
